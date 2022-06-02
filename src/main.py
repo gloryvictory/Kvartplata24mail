97b76b33edcf3ebbd914ed646e775be1de1c7fcc
ZVV <zamaraev@gmail.com>
2022-06-02 15:02:32 +0500

    add some config

--- src/main.py
+++ src/main.py
@@ -18,21 +18,16 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
+from datetime import datetime
+
 from core.logger import get_logger
 
 logger = get_logger()
 
-
+global root
 
 root = tk.Tk()
 apps = []
-
-if os.path.isfile('save.txt'):
-    with open('save.txt', 'r') as f:
-        tempApps = f.read()
-        tempApps = tempApps.split(',')
-        apps = [x for x in tempApps if x.strip()]
-        print(apps)
 
 # the new
 def addApp():
@@ -48,34 +43,53 @@ def addApp():
         label = tk.Label(frame, text=app, bg="gray")
         label.pack()
 
-
-def runApps():
+def run_apps():
     for app in apps:
         os.startfile(app)
 
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
-canvas.pack()
 
-frame = tk.Frame(root, bg="white")
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+def make_gui():
+    canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+    canvas.pack()
 
-openFile = tk.Button(root, text="Open File", padx=25,
-                     pady=10, fg='white', bg="#263D42", command=addApp)
-openFile.pack()
+    frame = tk.Frame(root, bg="white")
+    frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 
-runApps = tk.Button(root, text="Run Apps", padx=25,
-                    pady=10, fg='white', bg="#263D42", command=runApps)
-runApps.pack()
+    openFile = tk.Button(root, text="Open File", padx=25,
+                         pady=10, fg='white', bg="#263D42", command=addApp)
+    openFile.pack()
 
-for app in apps:
-    label = tk.Label(frame, text=app)
-    label.pack()
+    btn_run_apps = tk.Button(root, text="Run Apps", padx=25,
+                        pady=10, fg='white', bg="#263D42", command=run_apps)
+    btn_run_apps.pack()
 
-
-root.mainloop()
-
-
-with open('save.txt', 'w') as f:
     for app in apps:
-        f.write(app + ',')
+        label = tk.Label(frame, text=app)
+        label.pack()
+
+
+# class Root(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
+#         self.label.pack()
+
+
+
+def main():
+    time1 = datetime.now()
+    print('Starting at :' + str(time1))
+
+    make_gui()
+    # root = Root()
+    root.mainloop()
+
+    time2 = datetime.now()
+    print('Finishing at :' + str(time2))
+    print('Total time : ' + str(time2 - time1))
+    print('DONE !!!!')
+
+
+if __name__ == "__main__":
+    main()
